28a08e3cc8b7595501cb348f4010cd2a5aa65ec0
Reid <rlj0713@gmail.com>
2021-10-19 19:59:54 -0400

    fixed navigation for create link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,15 +2,28 @@ import React from 'react';
 
 export default class Navigation extends React.Component {
 
-    
+    displayCreate = () => {
+        if (window.location.href == "http://localhost:3001/" || window.location.href == "http://localhost:3001/reservations") {
+            return (
+                <a href="http://localhost:3001/reservations/new">Create Reservation</a>
+            )
+        } else if (window.location.href == "http://localhost:3001/guides") {
+            return (
+                <a href="http://localhost:3001/guides/new">Create Guide</a>
+            )
+        }
+    }
+
+
     render () {
         return(
             <nav className="nav">
                 <a href="http://localhost:3001/">Reservations</a>
+                {/* <a href="http://localhost:3001/reservations/new">Create Reservation</a> */}
                 <a href="http://localhost:3001/guides">Guides</a>
+                {/* <a href="http://localhost:3001/guides/new">Create Guide</a> */}
                 <a href="http://localhost:3001/customers">Customers</a>
-                <a class="navButton" href="http://localhost:3001/reservations/new">Create Reservation</a>
-                <a class="navButton" href="http://localhost:3001/guides/new">Create Guide</a>
+                {this.displayCreate()}
             </nav>
         )
     }
