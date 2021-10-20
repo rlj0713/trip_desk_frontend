40942372898214a08305287593696502f2ca843f
Reid <rlj0713@gmail.com>
2021-10-19 21:35:26 -0400

    added note

--- src/components/ReservationNew.js
+++ src/components/ReservationNew.js
@@ -34,6 +34,7 @@ class ReservationNew extends React.Component {
     }
   };
 
+  // GO back and fix date off by one bug
   handleDateChange(date) {
     this.setState({
       reservation_date: date,
