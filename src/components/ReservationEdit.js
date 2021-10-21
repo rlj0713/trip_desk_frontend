41440e0ee9c1b69a858a74839c0c27859e23098f
Reid <rlj0713@gmail.com>
2021-10-21 18:50:46 -0400

    populates edit res-customer dropdown

--- src/components/ReservationEdit.js
+++ src/components/ReservationEdit.js
@@ -2,38 +2,65 @@ import React from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { fetchCustomers } from '../actions/customerActions';
+import { connect } from 'react-redux';
 
 class ReservationEdit extends React.Component {
 
     constructor(props) {
-        super(props)
-        this.state = {
-            resObjectId: props.match.params.id,
-            resObject: {},
-            reservation_date: null
-        }
+      super(props)
+      this.state = {
+        resObjectId: props.match.params.id,
+        resObject: {},
+        reservation_date: null,
+        guide_id: null,
+        customer_id: null
+      }
+      // this.handleDateChange = this.handleDateChange.bind(this);
+      // this.handleGuideChange = this.handleGuideChange.bind(this);
+      this.handleCustomerChange = this.handleCustomerChange.bind(this);
+      // this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     // I need to make the old date appear in the form as the default value when rendering edit
     componentDidMount() {
-        const foundObject = this.reservationToEdit(this.state.resObjectId)
-        this.setState({
-            resObject: foundObject,
-            reservation_date: this.state.resObject.reservation_date
-        })
+      const foundObject = this.reservationToEdit(this.state.resObjectId)
+      this.setState({
+        resObject: foundObject,
+        reservation_date: this.state.resObject.reservation_date
+      })
+      this.props.fetchCustomersWithDispatch();
+    }
+
+    handleCustomerChange(e) {
+      e.preventDefault();
+      this.setState({
+        customer_id: e.target.value,
+      });
     }
     
-    reservationToEdit = (resObjectId) => {
-            return this.props.reservations.find(res => {
-                return res.id === this.state.resObjectId ? res : "hello world"
-            }
+    populateCustomerDropdown = () => {
+      console.log(this.props.reservations)
+      if (this.props.reservations) {
+        return(
+          this.props.reservations.map(res => 
+            <option value={res.customer.id}>{res.customer.first_name} {res.customer.last_name}</option>
+          )
         )
+      }
+    };
+    
+    reservationToEdit = (resObjectId) => {
+        return this.props.reservations.find(res => {
+          return res.id === this.state.resObjectId ? res : "hello world"
+        }
+      )
     }
 
     // Attempting to single out the object that is to be edited
-    render() {
-        return (
-            <form className="new-res" onSubmit={this.onFormSubmit}>
+  render() {
+    return (
+      <form className="new-res" onSubmit={this.onFormSubmit}>
         <DatePicker
         //   value={this.state.resObject.reservation_date}
           className="datepicker"
@@ -57,12 +84,18 @@ class ReservationEdit extends React.Component {
           className="input"
           onChange={this.handleCustomerChange}
         >
-          {/* {this.populateCustomerDropdown()} */}
+          {this.populateCustomerDropdown()}
         </select>
         <button type="submit">Update Reservation</button>
       </form>
-        )
-    }
+    )
+  }
 }
 
-export default ReservationEdit;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCustomersWithDispatch: () => dispatch(fetchCustomers())
+  }
+} 
+
+export default connect(null, mapDispatchToProps)(ReservationEdit);
